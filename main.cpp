5e5fe10c889429391db1bb0ac5900b8080db9b3d
Francis Giraldeau <francis.giraldeau@gmail.com>
2016-07-22 20:19:14 -0400

    Add more test cases
    taskisol [ default | signal ] [ syscall | pagefault ]
    
    default: Enable isolation with default (SIGKILL) behavior
    signal: Use SIGUSR1 instead of the default signal
    
    syscall: call write() after enabling isolation
    pagefault: access mmap() causing a page fault
    
    time ./taskisol default syscall
    time ./taskisol default pagefault
    time ./taskisol signal syscall
    time ./taskisol signal pagefault
    
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@
 #include <sys/prctl.h>
 #include <sys/syscall.h>
 
+/*
+ * Copied from linux/include/linux/isolation.h
+ */
 /* Enable/disable or query task_isolation mode for TASK_ISOLATION kernels. */
 #define PR_SET_TASK_ISOLATION		48
 #define PR_GET_TASK_ISOLATION		49
@@ -23,10 +26,10 @@ int gettid()
     return (int) syscall(SYS_gettid);
 }
 
-void sigkill(int signo)
+void sigusr1(int signo)
 {
     (void) signo;
-    const char *msg = "sigsegv\n";
+    const char *msg = "sigusr\n";
     write(2, msg, strlen(msg));
     exit(0);
 }
@@ -40,7 +43,7 @@ int main(int argc, char *argv[])
 
     cout << "taskisol run" << endl;
 
-    //signal(SIGKILL, sigkill);
+    signal(SIGUSR1, sigusr1);
 
     CPU_ZERO(&cpuset);
     CPU_SET(1, &cpuset);
@@ -55,26 +58,60 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    if (prctl(PR_SET_TASK_ISOLATION, PR_TASK_ISOLATION_ENABLE) < 0) {
-        perror("prctl");
-        return -1;
+    if (argc > 1) {
+
+        /*
+         * Default SIGKILL mode
+         */
+        if (strcmp(argv[1], "default") == 0) {
+            if (prctl(PR_SET_TASK_ISOLATION, PR_TASK_ISOLATION_ENABLE) < 0) {
+                perror("prctl default");
+                return -1;
+            }
+        }
+
+        /*
+         * The program completes when using USERSIG,
+         * but actually no signal is delivered
+         */
+        if (strcmp(argv[1], "signal") == 0) {
+            if (prctl(PR_SET_TASK_ISOLATION, PR_TASK_ISOLATION_USERSIG |
+                      PR_TASK_ISOLATION_SET_SIG(SIGUSR1)) < 0) {
+                perror("prctl sigusr");
+                return -1;
+            }
+        }
     }
 
-    if (argc > 1) {
-        if (strcmp(argv[1], "syscall") == 0) {
-            // does not produce a segfault, writes the string
+    if (argc > 2) {
+        if (strcmp(argv[2], "syscall") == 0) {
+            /*
+             * Does not trigger a SIGKILL, instead writes the string on
+             * stdout as normal
+             */
             const char *msg = "hallo\n";
             write(2, msg, strlen(msg));
         }
 
-        if (strcmp(argv[1], "pagefault") == 0) {
-            // does trigger SIGKILL
+        if (strcmp(argv[2], "pagefault") == 0) {
+            /*
+             * Triggers SIGKILL with default mode because the following
+             * store produces a pagefault, even though the memory is valid
+             */
             addr[0] = '\0';
         }
     }
 
-    for (int i = 0; i < 1E10; i++) {
+    /*
+     * Hog for a while to see if the task can survive (~10s on Intel i7)
+     */
+    for (long i = 0; i < 1E10; i++) {
         x = i;
     }
+
+    /*
+     * The task is allowed to call exit(), other syscall may produce SIGKILL.
+     */
+    exit(0);
     return 0;
 }
